5ff2cea26101034caae8a3694d93268d3e465b42
guimarangon <132426196+guimarangon@users.noreply.github.com>
2023-07-07 17:22:03 -0300

    Update external_data_link.py

--- external_data_link.py
+++ external_data_link.py
@@ -4,7 +4,7 @@ from database_connector import db, FinancialData
 FETCH_EXTERNAL_DATA = "FETCH_EXTERNAL_DATA"
 
 def fetch_external_data():
-    response = requests.get('https://external-data-source.com/financial-data')
+    response = requests.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vTGRs6Gx4xy7fSnganAALVC1hqzLw2Iyw9M0eOBkAMcVk2ypMmlP11ivuH9Df2PwA/pubhtml?gid=1728458610&single=true')
     data = response.json()
 
     for item in data:
